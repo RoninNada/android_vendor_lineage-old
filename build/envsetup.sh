c393df6bb1c4a630d5f466f955ff24f68d2af2a5
RoninNada <zerodev@gmx.com>
2023-03-05 13:35:56 +1000

    Change vendorsetup.sh reference in envsetup.sh to vendor/custom

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -69,7 +69,7 @@ function breakfast()
     LINEAGE_DEVICES_ONLY="true"
     unset LUNCH_MENU_CHOICES
     add_lunch_combo full-eng
-    for f in `/bin/ls vendor/lineage/vendorsetup.sh 2> /dev/null`
+    for f in `/bin/ls vendor/custom/vendorsetup.sh 2> /dev/null`
         do
             echo "including $f"
             . $f
